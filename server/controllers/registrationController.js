c7d552944fb3af15547701ceb72621a63af21128
Trina Haque <trina_haque28@outlook.com>
2017-05-04 14:08:16 -0700

    validations

--- server/controllers/registrationController.js
+++ server/controllers/registrationController.js
@@ -27,11 +27,11 @@ module.exports = {
 		var validationErrors = {};
 		req.session.errors = {};
 
-		if(req.body.first_name.length < 1){
+		if(req.body.first_name.length < 2){
 			valid = false;
 			validationErrors.first_name = "First name is required.";
 		}
-		if(req.body.last_name.length < 1){
+		if(req.body.last_name.length < 2){
 			valid = false;
 			validationErrors.last_name = "Last name is required.";
 		}
@@ -39,18 +39,37 @@ module.exports = {
 			valid = false;
 			validationErrors.phone = "Invalid phone number.";
 		}
+
 		if(req.body.email.length < 1){
 			valid = false;
 			validationErrors.email = "Email is required.";
 		}
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		var validateEmail = re.test(req.body.email);
 
-		if(req.body.pw1 !== req.body.pw2){
-			valid = false;
-			validationErrors.password = "Passwords do not match.";
+		if (!validateEmail){
+			validationErrors.email = "Email is not in email format";
 		}
-		else if(req.body.pw1.length < 8){
+
+		if(req.body.pw1.length < 8){
 			valid = false;
 			validationErrors.password = "Password must be at least 8 characters";
+		}
+		else{
+			if(req.body.pw1 !== req.body.pw2){
+				valid = false;
+				validationErrors.password = "Passwords do not match.";
+			}
+			else{
+				var hasUpperCase = /[A-Z]/.test(req.body.pw1);
+		    var hasLowerCase = /[a-z]/.test(req.body.pw1);
+		    var hasNumbers = /\d/.test(req.body.pw1);
+		    var hasNonalphas = /\W/.test(req.body.pw1);
+		    if (hasUpperCase + hasLowerCase + hasNumbers + hasNonalphas < 3){
+		        valid = false;
+		     		validationErrors.password = "Password needs a combination of upper-case letters, lower-case letters, numbers, and special characters";
+		    }
+			}
 		}
 
 		if(valid === true){
